ef4a8f5614834c394a69d01064597cf8b5ad30ec
Fiya Niswatus Sholihah <148621077+piyans@users.noreply.github.com>
2025-03-17 11:52:21 +0700

    Create stopwordsremoving.py

---
+++ stopwordsremoving.py
@@ -0,0 +1,25 @@
+import nltk
+from nltk.corpus import stopwords
+
+# Download stopwords jika belum diunduh
+nltk.download('stopwords')
+
+# Load stopwords untuk bahasa Indonesia
+stopwords_indonesian = stopwords.words('indonesian')
+
+# Daftar kata yang tetap diperlukan meskipun ada dalam stopwords
+needwords = ["tidak", "ada"]
+
+# Menghapus kata-kata yang diperlukan dari stopwords
+for word in needwords:
+    if word in stopwords_indonesian:
+        stopwords_indonesian.remove(word)
+
+# Fungsi untuk menghapus stopwords dari teks
+def remove_stopwords(text):
+    # Pisahkan teks menjadi kata-kata dan hapus stopwords serta kata yang panjangnya 1
+    text = [word for word in text.split() if len(word) > 1 and word.lower() not in stopwords_indonesian]
+    # Gabungkan kata-kata menjadi teks kembali
+    text = " ".join(text)
+    return text
+df['kalimat'] = df['kalimat'].apply(remove_stopwords)
