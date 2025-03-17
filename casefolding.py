b8a2933abae7a14359bdfde95386dad4baa88ad4
Fiya Niswatus Sholihah <148621077+piyans@users.noreply.github.com>
2025-03-17 11:58:30 +0700

    Delete casefolding.py

--- casefolding.py
+++
@@ -1,8 +0,0 @@
-# Melakukan lower casing
-df['kalimat'] = df['kalimat'].str.lower()
-# Menghapus tanda baca
-df['kalimat'] = df['kalimat'].str.replace('[^a-zA-Z0-9]', ' ')
-# Menghilangkan karakter tunggal
-df['kalimat'] = df['kalimat'].str.replace(r"\s+[a-zA-Z]\s+", ' ')
-# Menghilagkan spasi ganda
-df['kalimat'] = df['kalimat'].str.replace(r'[•\t|\n|\s+]', ' ')
